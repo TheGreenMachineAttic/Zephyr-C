3eb798303ea36e4e20f8b4ce4d8cc26a2e9e53b8
Cody A <Eapo101@yahoo.com>
2012-01-28 14:24:00 -0600

    added realays and changed my robot

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -6,27 +6,18 @@ class RobotDemo : public SimpleRobot
 {
 RobotDrive myRobot;
 GamepadL gamepad; 
-Joystick leftstick;
-Joystick rightstick;
-Jaguar leftjagFront; 
-Jaguar rightjagFront;
-Jaguar leftjagBack; 
-Jaguar rightjagBack;
 Jaguar shooter;
 Compressor comp;
+Relay rel;
 float speed;
 
 public:
 RobotDemo(void):
-myRobot(1, 2),
+myRobot(1, 2, 3, 4),
 gamepad(1),
-leftstick(1),
-rightstick(2),
-leftjagFront(1),
-rightjagFront(2),
-leftjagBack(3),
-rightjagBack(4),
-comp(3)
+shooter(5),
+comp(1, 1),
+rel(2)
 {
 	myRobot.SetExpiration(0.1);
 }
@@ -54,46 +45,16 @@ float RegulateSpeed (float speed)
 }
 
 void OperatorControl(void)
-{
+{ 
 	myRobot.SetSafetyEnabled(true);
 	while (IsOperatorControl())
 	{
-		myRobot.TankDrive(leftstick.GetRawAxis(1), rightstick.GetRawAxis(2)); 
+		myRobot.TankDrive(gamepad.GetLeftY(), gamepad.GetRightY()); 
 		RegulateSpeed(speed);
-		if (leftstick.GetY() == 1 && rightstick.GetY() == 1)
-		{
-			leftjagFront.Set(speed);
-			leftjagBack.Set(speed);
-			rightjagFront.Set(speed);
-			rightjagBack.Set(speed);
-		}
-		if (leftstick.GetY() == -1 || rightstick.GetY() == 1)
-		{
-			leftjagFront.Set(-speed);
-			leftjagBack.Set(-speed);
-			rightjagFront.Set(speed);
-			rightjagBack.Set(speed);
-		}
-		if (leftstick.GetY() == 1 || rightstick.GetY() == -1)
-		{
-			leftjagFront.Set(speed);
-			leftjagBack.Set(speed);
-			rightjagFront.Set(-speed);
-			rightjagBack.Set(-speed);
-		}
-		if (leftstick.GetY() == -1 && rightstick.GetY() == -1)
-		{
-			leftjagFront.Set(-speed);
-			leftjagBack.Set(-speed);
-			rightjagFront.Set(-speed);
-			rightjagBack.Set(-speed);
-		}
-		
-		comp.Start();
 		
 		if (gamepad.GetRawButton(7))
 		{
-			comp.Relay::kForward;
+			rel.Set(Relay::kForward);
 		}
 		
 		if (gamepad.GetRawButton(8))

66c862503b63ceacdee5ccf916f22ce87aed7c4b
Cody A <Eapo101@yahoo.com>
2012-02-10 20:38:10 -0600

    Created Drive and Shooter wrapper classes.

--- Components.h
+++ Components.h
@@ -1,17 +1,18 @@
-#include "GamePadL.h"
-#include "WPILib.h"
+
 
 #ifndef COMPONENTS_H
 #define COMPONENTS_H
-
+#include "GamePadL.h"
+#include "WPILib.h"
+#include "DriveComponents.h"
 /*
  * This contains the complete list of all components attached to the robot or driver station.
  */
 class Components{
 private:
 	//JAGUARS
-	static const int RIGHT_MOTOR = 1;
-	static const int LEFT_MOTOR = 2;
+	static int const  RIGHT_MOTOR = 1;
+	static int const  LEFT_MOTOR = 2;
 	static const int SHOOTER_JAGUAR_LEFT  = 3;
 	static const int SHOOTER_JAGUAR_RIGHT = 4; 
 	static const int SHOOTER_ROTATION = 5;
@@ -21,7 +22,7 @@ private:
 	//RELAYS
 	static const int COMP_INPUT = 1;
 	static const int PISTON_INPUT = 2;
-	static const int SUPER_SHIFTERS = 3;
+	static int const  SUPER_SHIFTER = 3;
 	static const int BALL_COLL_ROTAT = 4;
 	static const int BALL_COLL_LIFT = 5;
 	static const int CONV_MOV = 6;
@@ -30,35 +31,24 @@ private:
 	//DIGITAL INPUT
 	static const int COMP_DIGITAL_INPUT = 1;
 public:
-		//ROBOT
-		RobotDrive myRobot;
+		//Wrapper Classes
+		DrivingComponents drive;
 		//GAMEPADS
 		GamepadL gamepad1; 
 		GamepadL gamepad2;
-		//JAGUARS
-		Jaguar shooterRight;
-		Jaguar shooterLeft;
-		Jaguar shooterRotator;
 		//COMPRESSOR
 		Compressor comp;
 		//RELAYS
-		Relay shooterPiston;
-		Relay superShifters;
 		Relay collectorRotate;
 		Relay liftCollector;
 		Relay convMove;
 		//ANALOG INPUTS
 		AnalogChannel sonar;
 		Components():
-			myRobot(LEFT_MOTOR, RIGHT_MOTOR),
+			drive(LEFT_MOTOR, RIGHT_MOTOR, SUPER_SHIFTER),
 			gamepad1(GAMEPAD_INPUT_1),
 			gamepad2(GAMEPAD_INPUT_2),
-			shooterRight(SHOOTER_JAGUAR_RIGHT),
-			shooterLeft(SHOOTER_JAGUAR_LEFT),
-			shooterRotator(SHOOTER_ROTATION),
 			comp(COMP_DIGITAL_INPUT,COMP_INPUT),
-			shooterPiston(PISTON_INPUT),
-			superShifters(SUPER_SHIFTERS),
 			collectorRotate(BALL_COLL_ROTAT),
 			liftCollector(BALL_COLL_LIFT),
 			convMove(CONV_MOV),
---
+++ ShooterComponents.h
@@ -0,0 +1,42 @@
+#include "WPILib.h"
+
+#ifndef SHOOTER_COMPONENTS_H
+#define SHOOTER_COMPONENTS_H
+
+class ShooterComponents{
+
+private:
+Jaguar shooterLeft;
+Jaguar shooterRight;
+Jaguar shooterRotator;
+Relay shooterPiston;
+
+public:
+	ShooterComponents(int left, int right, int rotator, int piston):
+		shooterLeft(left),
+		shooterRight(right),
+		shooterRotator(rotator),
+		shooterPiston(piston)
+		{}
+	
+	void setShooterSpeed(float left, float right){
+		 shooterLeft.SetSpeed(left);
+		 shooterRight.SetSpeed(right);
+	}
+	
+	void setRotate(float rotator){
+		shooterRotator.SetSpeed(rotator);
+	}
+	
+	void piston(bool position){
+		if(state){
+			shooterPiston.Set(Relay::kReverse);
+		}
+		else{
+			shooterPiston.Set(Relay::kForward);
+		}
+	}
+
+};
+
+#endif

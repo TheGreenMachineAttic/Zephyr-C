f773d51fd5436228d58aee8ccc5ef7cd4a12078b
Danny Gratzer <danny.gratzer@gmail.com>
2012-02-14 20:19:55 -0600

    Commenting various classes.

--- Filter.cpp
+++ Filter.cpp
@@ -1,6 +1,8 @@
 #include "Filter.h"
 
-
+/*
+ * Creates a simple empty filter of size SIZE.
+ */
 Filter::Filter()
 {
 	for(int counter = 0; counter<SIZE; counter++)
@@ -8,7 +10,9 @@ Filter::Filter()
 		values[counter] = 0;
 	}
 }
-
+/*
+ * Shifts the array down by one, so values[0] would become values[1].
+ */
 void Filter::shiftArray()
 {
 	float tempArray[SIZE];
@@ -21,7 +25,9 @@ void Filter::shiftArray()
 			values[counter] = tempArray[counter-1];
 		}
 }
-
+/*
+ * Returns the FIR filtered value.
+ */
 float Filter::filter(float value)
 {
 	shiftArray();
